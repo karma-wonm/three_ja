8ef665c39f329da711d5461d207cbde0e8e246ee
Nastya Sizykh <sns0299@gmail.com>
2022-09-20 02:14:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int start, plus, end, thousand = 1000;
+        System.out.println("Введите сумму баланса счета до пополнения");
+        Scanner scanner = new Scanner(System.in);
+        start = scanner.nextInt();
+        System.out.println("Введите сумму, на которую хотите пополнить счёт");
+        Scanner scan = new Scanner(System.in);
+        plus = scanner.nextInt();
+        if(plus < thousand){
+            end = start + plus;
+            System.out.println("Баланс счета будет составлять " + end );
+        } else end = (plus/100) + start + plus;
+    System.out.println("Баланс счета будет составлять " + end);
+        System.out.println("Бонус составил " + plus/100);
+
+    }
+}
